09f34f6258fa8d7a476b5c9841cf08f342aa93f0
joelhaverinen <44695636+joelhaverinen@users.noreply.github.com>
2024-01-02 15:04:49 +0200

    Update mac_test.py

--- mac_test.py
+++ mac_test.py
@@ -6,13 +6,13 @@ sensor = RuuviTag("EC:28:C9:6E:9F:51")
 state = sensor.update() # will freeze here if sensor not found
 
 # print the current state
-print("Current State:", state)
+#print("Current State:", state)
 
 # get latest state (does not get it from the device)
 state = sensor.state
 
 # print the updated state
-print("Updated State:", state)
+#print("Updated State:", state)
 
 # Check if the MAC address is the expected one
 expected_mac_address = "ec28c96e9f51"
@@ -20,8 +20,8 @@ expected_mac_address = "ec28c96e9f51"
 
 data = state
 
-print(sensor)
-print(mac_address)
+#print(sensor)
+#print(mac_address)
 
 
 if state and 'mac' in state and state['mac'] == expected_mac_address: # täs muodos ec28c96e9f51 ei täs EC:28:C9:6E:9F:51
