711efca0603c975b5cc0fe690fc4480ba6ec7559
joelhaverinen <44695636+joelhaverinen@users.noreply.github.com>
2024-01-03 12:02:22 +0200

    Update mac_test.py
    Added timeout function.

--- mac_test.py
+++ mac_test.py
@@ -1,27 +1,29 @@
 import time
 from ruuvitag_sensor.ruuvitag import RuuviTag
+from timeout_decorator import timeout # pip install timeout-decorator
+
 
 sensor = RuuviTag("EC:28:C9:6E:9F:51")
 
-state = sensor.update() # will freeze here if sensor not found
+timeout_seconds = 1  # Aseta haluamasi timeout-aika sekunteina
 
-# print the current state
-#print("Current State:", state)
+@timeout(timeout_seconds)
+def update_sensor():
+    return sensor.update()
 
+try:
+    state = update_sensor()
+except TimeoutError:
+    print('Timeout occurred while updating sensor.')
+
+    
 # get latest state (does not get it from the device)
 state = sensor.state
-
-# print the updated state
-#print("Updated State:", state)
 
 # Check if the MAC address is the expected one
 expected_mac_address = "ec28c96e9f51"
 
-
 data = state
-
-#print(sensor)
-#print(mac_address)
 
 
 if state and 'mac' in state and state['mac'] == expected_mac_address: # täs muodos ec28c96e9f51 ei täs EC:28:C9:6E:9F:51
